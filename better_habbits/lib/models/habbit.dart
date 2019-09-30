7f93bea7ca6b3363afdf496cef19ce644d283ee2
andre <brauni.andre@gmail.com>
2019-09-30 17:00:07 +0200

    add first habbit implementation

---
+++ better_habbits/lib/models/habbit.dart
@@ -0,0 +1,28 @@
+
+final String columnId = '_id';
+final String columnName = 'name';
+
+class Habbit {
+
+  int id;
+  String name;
+  String category;
+
+  Habbit();
+
+  Habbit.fromMap(Map<String, dynamic> map) {
+    id = map[columnId];
+    name = map[columnName];
+  }
+
+  Map<String, dynamic> toMap() {
+    var map = <String, dynamic>{
+      columnName: name
+    };
+    if (id != null) {
+      map[columnId] = id;
+    }
+    return map;
+  }
+
+}
