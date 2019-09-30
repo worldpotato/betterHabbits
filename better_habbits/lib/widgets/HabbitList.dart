2e45b58fb2a68099f0335f4d7363bbf3afb8b106
andre <brauni.andre@gmail.com>
2019-09-30 19:09:05 +0200

    changes in habbibilist -> first run on list

--- better_habbits/lib/widgets/HabbitList.dart
+++ better_habbits/lib/widgets/HabbitList.dart
@@ -1,6 +1,8 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:better_habbits/models/habbit.dart';
+import 'package:better_habbits/utils/databaseHelper.dart';
 
 class HabbitList extends StatefulWidget {
   final name = "Better Habbits";
@@ -10,16 +12,25 @@ class HabbitList extends StatefulWidget {
 }
 
 class HabbitListState extends State<HabbitList> {
+
+  List<Habbit> habbits = null;
+
+  _readHabbits() async {
+    DatabaseHelper helper = DatabaseHelper.instance;
+    habbits = await helper.queryAllHabbits();
+  }
+
   @override
   Widget build(BuildContext context) {
+    _readHabbits();
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.name),
       ),
       body: ListView.builder(
-        itemCount: 50,
+        itemCount: habbits.length,
         itemBuilder: (BuildContext ctxt, int index) =>
-            HabbitPreviewBuilder().build(),
+            HabbitPreviewBuilder().build(habbits[index]),
       ),
     );
   }
@@ -28,10 +39,12 @@ class HabbitListState extends State<HabbitList> {
 class HabbitPreviewBuilder {
   num maxHeight = 150.0;
 
-  Widget build() {
+  Widget build(Habbit hab) {
     return LimitedBox(
       maxHeight: maxHeight,
       child: Container(
+        height: 100,
+        child: Text(hab.name),
         color: Color.fromARGB(
             Random.secure().nextInt(255),
             Random.secure().nextInt(255),
