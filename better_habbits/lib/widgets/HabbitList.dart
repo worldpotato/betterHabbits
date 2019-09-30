d750b0a8ab95f8f7a97668cbb633b22606859e55
Felix Strobel <mail@felixstrobel.de>
2019-09-30 18:27:29 +0200

    use listView.builder to build the list

--- better_habbits/lib/widgets/HabbitList.dart
+++ better_habbits/lib/widgets/HabbitList.dart
@@ -7,44 +7,36 @@ class HabbitList extends StatefulWidget {
 
   @override
   State<StatefulWidget> createState() => HabbitListState();
-
 }
 
-class HabbitListState extends State<HabbitList>{
-
+class HabbitListState extends State<HabbitList> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.name),
       ),
-      body: ListView(
-        children: <Widget>[
-          LimitedBox(
-            maxHeight: 200,
-            child: Container(
-              color: Color.fromARGB(Random.secure().nextInt(255), Random.secure().nextInt(255), Random.secure().nextInt(255), Random.secure().nextInt(255)),
-            ),
-          ),
-          LimitedBox(
-            maxHeight: 200,
-            child: Container(
-              color: Color.fromARGB(Random.secure().nextInt(255), Random.secure().nextInt(255), Random.secure().nextInt(255), Random.secure().nextInt(255)),
-            ),
-          ),
-          LimitedBox(
-            maxHeight: 200,
-            child: Container(
-              color: Color.fromARGB(Random.secure().nextInt(255), Random.secure().nextInt(255), Random.secure().nextInt(255), Random.secure().nextInt(255)),
-            ),
-          ),
-          LimitedBox(
-            maxHeight: 200,
-            child: Container(
-              color: Color.fromARGB(Random.secure().nextInt(255), Random.secure().nextInt(255), Random.secure().nextInt(255), Random.secure().nextInt(255)),
-            ),
-          ),
-        ],
+      body: ListView.builder(
+        itemCount: 50,
+        itemBuilder: (BuildContext ctxt, int index) =>
+            HabbitPreviewBuilder().build(),
+      ),
+    );
+  }
+}
+
+class HabbitPreviewBuilder {
+  num maxHeight = 150.0;
+
+  Widget build() {
+    return LimitedBox(
+      maxHeight: maxHeight,
+      child: Container(
+        color: Color.fromARGB(
+            Random.secure().nextInt(255),
+            Random.secure().nextInt(255),
+            Random.secure().nextInt(255),
+            Random.secure().nextInt(255)),
       ),
     );
   }
